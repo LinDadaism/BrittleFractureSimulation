210a49687e7dcf46242a744c10d2366d20444e4b
szleo123 <szleo123@users.noreply.github.com>
2024-03-27 01:24:17 -0400

    finish clipping

--- src/clipper.cpp
+++ src/clipper.cpp
@@ -3,7 +3,7 @@
 Pattern::Pattern(AllCellVertices v, AllCellFaces f, AllCellEdges e):o_cellVertices(v), o_cellFaces(f), o_cellEdges(e)
 {}
 
-std::vector<Cell> Pattern::getCells() {
+std::vector<Pattern::sPCell> Pattern::getCells() {
     return cells;
 }
 
@@ -22,9 +22,67 @@ void Pattern::createCellsfromVoro() {
             Eigen::Vector3d v2 = p3 - p1; 
             Eigen::Vector3d normal = v1.cross(v2); 
             double d = normal.dot(p1); 
-            planes.push_back(Plane{ -normal, -d});
+            planes.push_back(Plane{ normal, d});
         }
-        cells.push_back(Cell{ planes, std::vector<MeshConvex>{} });
+        Surface_mesh sm; 
+        buildSMfromVF(o_cellVertices[i], o_cellFaces[i], sm);
+        sPCell cellptr(new Cell{ planes, std::vector<int>{}, sm });
+        cells.push_back(cellptr);
+    }
+}
+
+void buildSMfromVF(const std::vector<Eigen::Vector3d>& vertices, const std::vector<std::vector<int>>& faces, Surface_mesh& sm) {
+    sm.clear();
+    // put in all vertices 
+    for (const auto& v : vertices) {
+        sm.add_vertex(Point_3(v.x(), v.y(), v.z()));
+    }
+    // put in all faces 
+    for (const auto& f : faces) {
+        std::vector<vertex_descriptor> indices; 
+        for (int i : f) {
+            indices.push_back(vertex_descriptor(i));
+        }
+        sm.add_face(indices);
+    }
+    // triangualte all non-triangle faces
+    PMP::triangulate_faces(sm);
+}
+
+void buildVFfromSM(const Surface_mesh& sm, std::vector<Eigen::Vector3d>& vertices, std::vector<std::vector<int>>& faces) {
+    vertices.clear(); 
+    faces.clear();
+    for (const auto& v : sm.vertices()) {
+        const Point_3& p = sm.point(v);
+        vertices.push_back(Eigen::Vector3d(p.x(), p.y(), p.z()));
+    }
+    for (const auto& f : sm.faces()) {
+        std::vector<int> face;
+        for (auto vd : vertices_around_face(sm.halfedge(f), sm)) {
+            face.push_back(vd.idx());
+        }
+        faces.push_back(face);
+    }
+}
+
+void calculateCentroid(MeshConvex& mesh, Eigen::Vector3d center) {
+    auto sm = mesh.convexMesh;
+    K::Vector_3 com(0.0, 0.0, 0.0); 
+    K::Vector_3 old_center(center.x(), center.y(), center.z());
+    int count = 0; 
+    for (auto v : sm.vertices()) {
+        com = com + (sm.point(v) - CGAL::ORIGIN - old_center);
+        ++count;
+    }
+    if (count != 0) {
+        com = com / count;
+    }
+    mesh.centroid = Eigen::Vector3d(com.x(), com.y(), com.z());
+}
+
+void translateMesh(MeshConvex& mesh, Eigen::Vector3d direction, double scale) {
+    for (size_t i = 0; i < mesh.vertices.size(); i++) {
+        mesh.vertices[i] += direction.normalized() * scale;
     }
 }
 
@@ -72,10 +130,56 @@ MeshConvex clipConvexAgainstPlane(const MeshConvex& convex, const Plane& plane) 
     return MeshConvex{ resultVertices, resultFaces };
 }
 
+MeshConvex clipConvexAgainstPlane2(const MeshConvex& convex, const Plane& plane) {
+    MeshConvex convex_copy = convex; 
+    Plane_3 p(plane.normal.x(), plane.normal.y(), plane.normal.z(), plane.distance); 
+    Surface_mesh convexm = convex_copy.convexMesh;
+    bool check = PMP::clip(convexm, p, PMP::parameters::clip_volume(true).use_compact_clipper(true));
+    convexm.collect_garbage();
+    std::vector<Eigen::Vector3d> vertices; 
+    std::vector<std::vector<int>> faces; 
+    buildVFfromSM(convexm, vertices, faces);
+    return MeshConvex{ vertices, faces, convexm };
+}
+
+
 MeshConvex clipConvexAgainstCell(const MeshConvex& convex, const Cell& cell) {
     MeshConvex curMesh = convex;
     for (const auto& plane : cell.planes) {
         curMesh = clipConvexAgainstPlane(curMesh, plane);
     }
     return curMesh;
+}
+
+MeshConvex clipConvexAgainstCell2(const MeshConvex& convex, const Cell& cell) {
+    // use the copy to do the clipping as cgal clipping modifies on the original meshes
+    MeshConvex convex_copy = convex;
+    Cell       cell_copy = cell; 
+    Surface_mesh convexm = convex_copy.convexMesh; 
+    Surface_mesh cellm   = cell_copy.cellMesh;
+    bool a = PMP::does_self_intersect(convexm);
+    bool b = PMP::does_self_intersect(cellm);
+    //while (a) {
+    //    Surface_mesh::Halfedge_index hole_he_index;
+    //    for (auto h : convexm.halfedges()) {
+    //        if (convexm.is_border(h)) {
+    //            hole_he_index = h;
+    //            break; // Found a boundary halfedge, break the loop
+    //        }
+    //    }
+    //    std::vector<Surface_mesh::Face_index>  patched_faces;
+    //    PMP::triangulate_hole(mesh,
+    //        hole_he_index,
+    //        std::back_inserter(patched_faces),
+    //        CGAL::parameters::vertex_point_map(mesh.points()).geom_traits(Kernel()));
+    //}
+    //PMP::clip(cellm, convexm, PMP::parameters::clip_volume(true), PMP::parameters::use_compact_clipper(true));
+    PMP::clip(convexm, cellm, PMP::parameters::clip_volume(true).use_compact_clipper(true));
+    //Surface_mesh result; 
+    //PMP::corefine_and_compute_intersection(convexm, cellm, result);
+    convexm.collect_garbage();
+    std::vector<Eigen::Vector3d> vertices; 
+    std::vector<std::vector<int>> faces; 
+    buildVFfromSM(convexm, vertices, faces);
+    return MeshConvex{ vertices, faces, convexm };
 }
--- src/clipper.h
+++ src/clipper.h
@@ -5,18 +5,29 @@
 #include <Eigen/Sparse>
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Plane_3.h>
 #include <CGAL/Polyhedron_3.h>
 #include <CGAL/Surface_mesh.h>
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/convex_hull_3.h>
+#include <CGAL/Polygon_mesh_processing/triangulate_faces.h>
+#include <CGAL/Polygon_mesh_processing/corefinement.h>
+#include <CGAL/Polygon_mesh_processing/measure.h>
+#include <CGAL/Polygon_mesh_processing/repair.h>
+#include <CGAL/Polygon_mesh_processing/clip.h>
+#include <CGAL/centroid.h>
 #include <vector>
 #include <memory>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel  K;
-typedef CGAL::Polyhedron_3<K>                     Polyhedron_3;
-typedef K::Point_3                                Point_3;
-typedef K::Vector_3                               Vector_3;
-typedef CGAL::Surface_mesh<Point_3>               Surface_mesh;
+typedef CGAL::Polyhedron_3<K>                                Polyhedron_3;
+typedef K::Point_3                                           Point_3;
+typedef K::Vector_3                                          Vector_3;
+typedef K::Plane_3                                           Plane_3;
+typedef CGAL::Surface_mesh<Point_3>                          Surface_mesh;
+typedef Surface_mesh::Vertex_index                                   vertex_descriptor;
+typedef Surface_mesh::Face_index                                     face_descriptor;
+namespace PMP = CGAL::Polygon_mesh_processing;
 
 struct Plane {
     Eigen::Vector3d normal;
@@ -33,12 +44,15 @@ struct Plane {
 struct MeshConvex {
     std::vector<Eigen::Vector3d> vertices;  // Assuming convex shape is represented by its vertices
     std::vector<std::vector<int>> faces; 
+    Surface_mesh convexMesh;                // convenient for cgal operations  
+    Eigen::Vector3d centroid{0, 0, 0};               // center of mass of a set of points
 };
 
 // representing a convex piece of the fracture pattern
 struct Cell {
     std::vector<Plane> planes;
-    std::vector<MeshConvex> convexes; // Store intersected pieces 
+    std::vector<int> convexes; // Store convex index for intersected pieces 
+    Surface_mesh cellMesh;            // convenient for cgal operations  
 };
 
 class Pattern 
@@ -46,13 +60,14 @@ class Pattern
     typedef std::vector<std::vector<Eigen::Vector3d>>  AllCellVertices;
     typedef std::vector<std::vector<std::vector<int>>> AllCellFaces;
     typedef std::vector<Eigen::MatrixXi>               AllCellEdges; 
+    typedef std::shared_ptr<Cell>                      sPCell;
 
 public:
     Pattern(AllCellVertices, AllCellFaces, AllCellEdges);
     ~Pattern() {};
 
     void createCellsfromVoro();
-    std::vector<Cell> getCells(); 
+    std::vector<sPCell> getCells();
 
 private:
     // original data outputed from Voronoi function of libigl
@@ -60,10 +75,20 @@ private:
     AllCellFaces o_cellFaces;
     AllCellEdges o_cellEdges;
     // data converted for mesh clipping and convex hull algorithm
-    std::vector<Cell> cells;
+    std::vector<sPCell> cells;
 
 };
-MeshConvex clipConvexAgainstPlane(const MeshConvex& convex, const Plane& plane);
-MeshConvex clipConvexAgainstCell(const MeshConvex& convex, const Cell& cell);
 
+// convert vertices+faces data into triangulated Surface_mesh data
+void buildSMfromVF(const std::vector<Eigen::Vector3d>&, const std::vector<std::vector<int>>&, Surface_mesh&);
+// convert triangulated Surface_mesh data into vertices+faces data
+void buildVFfromSM(const Surface_mesh&, std::vector<Eigen::Vector3d>&, std::vector<std::vector<int>>&);
+// calculate the centroid of a MeshConvex
+void calculateCentroid(MeshConvex& mesh, Eigen::Vector3d com); 
+// modify vertices in MeshConvex by a vector 
+void translateMesh(MeshConvex& mesh, Eigen::Vector3d direction, double scale);
+MeshConvex clipConvexAgainstPlane(const MeshConvex& convex, const Plane& plane);
+MeshConvex clipConvexAgainstPlane2(const MeshConvex& convex, const Plane& plane);
+MeshConvex clipConvexAgainstCell(const MeshConvex& convex, const Cell& cell);
+MeshConvex clipConvexAgainstCell2(const MeshConvex& convex, const Cell& cell);
 #endif // !MESH_CLIPPER
