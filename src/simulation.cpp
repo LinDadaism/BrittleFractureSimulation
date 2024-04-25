383968f2dd3929748f5b17a917f20978f64e9242
Linda Zhu <lindadaism@users.noreply.github.com>
2024-04-24 23:29:42 -0400

    add back dynamic Voronoi pattern generation

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -5,18 +5,14 @@ Simulation::Simulation()
 }
 
 Simulation::Simulation(std::string filepath) :
-    coacdMeshFilepath(filepath),
-    pattern(gCellVertices10, gCellFaces10, gCellEdges10)
+    coacdMeshFilepath(filepath)
 {
-    //pattern.createCellsfromVoro();
 }
 
 Simulation::Simulation(vec3 pos, float r, float amt, std::string filepath) :
 	impactPos(pos), impactRadius(r), explodeAmt(amt),
-	coacdMeshFilepath(filepath),
-    pattern(gCellVertices10, gCellFaces10, gCellEdges10)
+	coacdMeshFilepath(filepath)
 {
-    //pattern.createCellsfromVoro();
 }
 
 std::string Simulation::getInputFilepath()
@@ -39,17 +35,33 @@ void Simulation::setInputFilepath(std::string filepath)
 	coacdMeshFilepath = filepath;
 }
 
-void Simulation::genFractureUniform(const std::vector<vec3>& nodes, vec3 minCorner, vec3 maxCorner)
+void Simulation::genFractureUniformStatic()
 {
-    //AllCellVertices cellVertices;   // Vertices of each Voronoi cell
-    //AllCellFaces cellFaces;         // Faces of each Voronoi cell represented by vertex indices
-    //AllCellEdges cellEdges;         // Edges of each Voronoi cell represented by vertex indices
-    //computeVoronoiCells(nodes, minCorner, maxCorner, cellVertices, cellFaces, cellEdges);
-
+    pattern.setVertices(gCellVertices10);
+    pattern.setFaces(gCellFaces10);
     pattern.createCellsfromVoro();
     
     auto convexes = readOBJByComponents(coacdMeshFilepath);
    
+    Eigen::Vector3d centroid = calculateCentroidCompound(convexes);
+    Compound original{ convexes, centroid };
+
+    fractureShards = fracturePipeline(original, pattern);
+}
+
+void Simulation::genFractureUniformDynamic(const std::vector<vec3>& nodes, vec3 minCorner, vec3 maxCorner)
+{
+    AllCellVertices cellVertices;   // Vertices of each Voronoi cell
+    AllCellFaces cellFaces;         // Faces of each Voronoi cell represented by vertex indices
+    AllCellEdges cellEdges;         // Edges of each Voronoi cell represented by vertex indices
+    computeVoronoiCells(nodes, minCorner, maxCorner, cellVertices, cellFaces, cellEdges);
+
+    pattern.setVertices(cellVertices);
+    pattern.setFaces(cellFaces);
+    pattern.createCellsfromVoro();
+
+    auto convexes = readOBJByComponents(coacdMeshFilepath);
+
     // Using weighted sum to approximate compound's CoM
     // Some of the original convex hulls might be overlapping so this is just an approximate.
     Eigen::Vector3d centroid = calculateCentroidCompound(convexes);
